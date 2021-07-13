443dfc5407c910eca470d05384448742a03dbdda
Monisha Basak <74670629+MonishaBasak@users.noreply.github.com>
2021-07-13 13:08:50 +0600

    Add files via upload
    Arduino code for moisture sensing

---
+++ moisture.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = digitalRead(8);
+  sensorValue= constrain(sensorValue,485,1023);
+  
+  Serial.println(sensorValue);
+  delay(1000);
+}
